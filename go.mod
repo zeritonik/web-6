afa3722828e388a9d7662a3eb910032ef0192354
ValeryBMSTU <30847068+ValeryBMSTU@users.noreply.github.com>
2024-09-29 16:45:49 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ValeryBMSTU/web-6
+
+go 1.22.1
---
+++ projects/3_count/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	// "strconv" // вдруг понадобится ;)
+)
+
+func main() {
+	fmt.Println("Hi, Bro!!! 🥴")
+}
